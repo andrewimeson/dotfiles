cb6d749f55d369c997a41ccb341a1b76bc2d2b0e
Andrew Imeson <andrew@andrewimeson.com>
2022-03-31 14:29:07 -0400

    Add support for bat (a cat with syntax highlighting)

---
+++ dotfiles/zsh_custom/color_preferences.zsh
@@ -0,0 +1,10 @@
+# This could be better as an associative array (does zsh have those?) or a case
+# statement, but this is practical
+if [[ $ITERM_PROFILE == 'Solarized-Light' ]]; then
+  export BAT_THEME='Solarized (light)'
+# I don't actually have a dark theme right now
+elif [[ $ITERM_PROFILE == 'Solarized-Dark' ]]; then
+  export BAT_THEME='Solarized (dark)'
+else
+  export BAT_THEME='ansi'
+fi
