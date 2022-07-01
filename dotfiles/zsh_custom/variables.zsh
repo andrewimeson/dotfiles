3ed6d59ab2a1b5a90daf4c57dd9eb3705f271b27
Andrew Imeson <andrew@andrewimeson.com>
2022-07-01 18:54:24 -0400

    Workaround Podman Docker Buildkit issue

--- dotfiles/zsh_custom/variables.zsh
+++ dotfiles/zsh_custom/variables.zsh
@@ -5,3 +5,5 @@ cbFamFilter=185.228.168.168
 cbAdultFilter=185.228.168.10
 
 chromeUserAgentMac='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+# Workaround for https://github.com/containers/podman/issues/13889
+export DOCKER_BUILDKIT=0
