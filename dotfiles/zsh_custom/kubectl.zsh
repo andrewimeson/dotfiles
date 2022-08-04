42ca90a95dc1de0553786ab9ae525ca5bf2b19db
Andrew Imeson <andrew@andrewimeson.com>
2022-08-04 18:46:51 -0400

    Update vendored oh-my-zsh kubectl completion
    https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/kubectl/kubectl.plugin.zsh

--- dotfiles/zsh_custom/kubectl.zsh
+++ dotfiles/zsh_custom/kubectl.zsh
@@ -1,13 +1,12 @@
 # Taken from oh-my-zsh plugin, but I didn't want all the aliases
 if (( $+commands[kubectl] )); then
-    __KUBECTL_COMPLETION_FILE="${ZSH_CACHE_DIR}/kubectl_completion"
-
-    if [[ ! -f $__KUBECTL_COMPLETION_FILE ]]; then
-        kubectl completion zsh >! $__KUBECTL_COMPLETION_FILE
-    fi
-
-    [[ -f $__KUBECTL_COMPLETION_FILE ]] && source $__KUBECTL_COMPLETION_FILE
-
-    unset __KUBECTL_COMPLETION_FILE
+  # If the completion file does not exist, generate it and then source it
+  # Otherwise, source it and regenerate in the background
+    if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
+        kubectl completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubectl" >/dev/null
+        source "$ZSH_CACHE_DIR/completions/_kubectl"
+    else
+        source "$ZSH_CACHE_DIR/completions/_kubectl"
+        kubectl completion zsh | tee "$ZSH_CACHE_DIR/completions/_kubectl" >/dev/null &|
+  fi
 fi
-
