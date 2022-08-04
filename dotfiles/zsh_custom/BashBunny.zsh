43b4b27b3bdf4535e1d5e963b9bbc4fa67ac728c
Andrew Imeson <andrew@andrewimeson.com>
2022-08-04 18:30:43 -0400

    Rewrite BashBunny path setting as an anonymous function

--- dotfiles/zsh_custom/BashBunny.zsh
+++ dotfiles/zsh_custom/BashBunny.zsh
@@ -1,10 +1,13 @@
 # I needed to set the environment variable to update the Bash bunny using the
-# supplied firemware update tool from Hak5. I'm guessing macOS security
+# supplied firmware update tool from Hak5. I'm guessing macOS security
 # improvements broke this, which is hilarious
 
 # https://forums.hak5.org/topic/50804-bashbunny-updater-needs-help-on-macos-catalina/
 
-EXPECTED_LOCATION=/Volumes/BashBunny
-if [[ -d $EXPECTED_LOCATION ]]; then
-    export BUNNYPATH=$EXPECTED_LOCATION
-fi
+# Yay, anonymous functions!
+() {
+    local _EXPECTED_LOCATION=/Volumes/BashBunny
+    if [[ -f $_EXPECTED_LOCATION ]]; then
+        export BUNNYPATH=$_EXPECTED_LOCATION
+    fi
+}
