6caab1d6790549ce95f6030d5373dbb949f48326
Andrew Imeson <andrew@andrewimeson.com>
2022-08-04 18:21:42 -0400

    Set Podman as the "Docker" host

--- dotfiles/zsh_custom/variables.zsh
+++ dotfiles/zsh_custom/variables.zsh
@@ -7,3 +7,5 @@ cbAdultFilter=185.228.168.10
 chromeUserAgentMac='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 # Workaround for https://github.com/containers/podman/issues/13889
 export DOCKER_BUILDKIT=0
+# Set Podman as the Docker daemon
+export DOCKER_HOST="unix://$HOME/.local/share/containers/podman/machine/podman-machine-default/podman.sock"
